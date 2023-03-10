2ffc2cd9e6e119146da183a7bfc181ea39fc444b
Dmytro <ihnatenko.dev@gmail.com>
2023-03-10 18:08:45 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+require('electron-reload')(__dirname);
+
+const createWindow = () => {
+	const mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+		},
+	});
+
+	mainWindow.loadFile(path.join(__dirname, 'index.html'));
+	mainWindow.webContents.openDevTools();
+};
+
+app.whenReady().then(() => {
+	createWindow();
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
+});
